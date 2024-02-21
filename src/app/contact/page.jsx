5f47d3cbf870726d53b8ee513fb7255ba68f393a
Renata Souza <77476383+renata-souza@users.noreply.github.com>
2024-02-20 23:47:04 -0300

    feat: create contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
+import styles from './contact.module.css'
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <main className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="/contact.png" alt='illustration of a man pointing to a formulary' fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form} action="">
+          <input type="text" placeholder='Full name' />
+          <input type="email" placeholder='Email address' />
+          <input type="text" placeholder='Phone Number (optional)' />
+          <textarea cols="30" rows="10" placeholder='Message'></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
